c0baec7a2c5984948f1d9b2f29738be0b8ed2a59
fielnyrb <brynleif@brynleif.com>
2021-09-12 15:43:20 +0100

    Game Over

--- North Pole Diving Experience/Assets/Scripts/StatusControl.cs
+++ North Pole Diving Experience/Assets/Scripts/StatusControl.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class StatusControl : MonoBehaviour
 {
@@ -56,6 +57,7 @@ public class StatusControl : MonoBehaviour
         if(oxygenBar.fillAmount <= 0)
         {
             StartCoroutine(FadeToBlack());
+            SceneManager.LoadScene("GameOver");
         }
     }
 
